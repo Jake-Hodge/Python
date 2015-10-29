0838e901cde74dfffc676e273d04c6a0a07b6dbf
Jacob Hodge <J-Mac@MacBook-Pro.home>
2015-10-29 23:04:09 +0000

    Last website visited working. Now needs to be tweeted

--- TwitterThing/autoTweet.py
+++ TwitterThing/autoTweet.py
@@ -4,21 +4,34 @@ import twitter, datetime
 file = open("TwitterCredentials.txt")
 creds = file.readline().strip().split(',')
 
-#/Users/J-Mac/Library/Application Support/Google/Chrome/Default
-#/Users/J-Mac/Library/Application\ Support/Google/Chrome/Default/Current\ Session 
-history = open("/Users/J-Mac/Library/Application Support/Google/Chrome/Default/Current Session")
+#Create a new API wrapper, passing in credentials one at a time
+api = twitter.Api(creds[0], creds[1],creds[2],creds[3])
 
+#/Users/J-Mac/Library/Application Support/Google/Chrome/Default (Current sesh path)
+
+history = open("/Users/J-Mac/Library/Application Support/Google/Chrome/Default/Current Session")
 histFull = history.read()
 
+first = "https://"
+second = "/"
+
+# Starts from the beginning of txt file and goes in order
+"""start = histFull.find(first) + len(first) 
+finish = histFull.find(second, start)"""
+
+#Starts at the end of txt file and works in reverse. Makes sense to use this one
+finish = histFull.rfind(second)
+start = histFull.rfind(first, 0, finish - len(first)) + len(first)
+latestSite = (histFull[start:finish])
+print(latestSite)
 
 
-print(histFull)
-#Create a new API wrapper, passing in my credentials one at a time
-api = twitter.Api(creds[0], creds[1],creds[2],creds[3])
+
+
 
 
 """ TO DO:
-find http
+find http (look up Find on Python docs)
 find slash
 convert url to page title
 """
